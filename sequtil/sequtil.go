1101205230c65e1250fd67e3dd486f562560959e
Brian Hall <bhall7@hawaii.edu>
2014-02-21 16:30:52 -1000

    passed that test

--- sequtil/sequtil.go
+++ sequtil/sequtil.go
@@ -1,5 +1,12 @@
 package sequtil
 
+import "strings"
+
 func FindFlankingSeqs(seq string, flankers [][]string) []string {
-	return flankers[0]
+	for _, pair := range flankers {
+		if (strings.HasPrefix(seq, pair[0]) && strings.HasSuffix(seq, pair[1])) {
+			return pair
+		}
+	}
+	return nil
 }
