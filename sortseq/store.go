a2ac23367a07033e32184e8c67e69046fd33e867
Theodore DeRego <tderego94@gmail.com>
2014-03-05 10:40:30 -1000

    SortedSeq struct

--- sortseq/store.go
+++ sortseq/store.go
@@ -6,3 +6,14 @@ import (
 type Store struct {
     fileName string
 }
+
+func NewStore(fileName string) *Store {
+    s := &Store{}
+
+    s.fileName = fileName
+
+    return s
+}
+
+func (s *Store) AddSeq(seq SortedSeq) {
+}
