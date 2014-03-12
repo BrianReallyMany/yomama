634ec37b16cbb8fd7886caa378bc3e7c2a2f0073
Brian Hall <bhall7@hawaii.edu>
2014-03-11 15:32:04 -1000

    removed SortedSeq struct entirely

--- seq/sortseq/sortedseq.go
+++ seq/sortseq/sortedseq.go
@@ -1,16 +1,7 @@
 package sortseq
 
-import (
-    . "github.com/BrianReallyMany/yomama/seq"
-)
-
 type SortKey struct {
     Locus  string
     Sample string
 }
 
-// SortedSeq is the type that represents a genetic sequence sorted by primer, barcode, and linker.
-type SortedSeq struct {
-    Seq
-    SortKey
-}
