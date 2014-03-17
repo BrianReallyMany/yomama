da418731bdcaeb7e7b8747d4877126e4b761b7b3
Theodore DeRego <tderego94@gmail.com>
2014-03-17 09:43:07 -1000

    FastqReader + unit tests

---
+++ iomama/fastq/fastq_reader.go
@@ -0,0 +1,60 @@
+package fasta
+
+import (
+    "bufio"
+    "bytes"
+    . "github.com/BrianReallyMany/yomama/seq"
+    "io"
+)
+
+type FastqReader struct {
+    reader *bufio.Reader
+
+    next [3][]byte // Stores next 3 meaningful lines. Used for iteration
+}
+
+func NewFastqReader(reader *bufio.Reader) *FastqReader {
+    f := &FastqReader{}
+    f.reader = reader
+
+    return f
+}
+
+// Returns whether or not there is another sequence to be grabbed. Used for iteration.
+func (f *FastqReader) HasNext() bool {
+    var err error
+
+    // Grab next two lines, verify, and return false if any errors occur.
+
+    f.next[0], err = f.reader.ReadBytes('\n')
+    if err != nil || len(f.next[0]) == 0 || f.next[0][0] != '@' {
+        return false
+    }
+
+    f.next[1], err = f.reader.ReadBytes('\n')
+    if err != nil {
+        return false
+    }
+
+    plusLine, err := f.reader.ReadBytes('\n')
+    if err != nil || len(plusLine) == 0 || plusLine[0] != '+' {
+        return false
+    }
+
+    f.next[2], err = f.reader.ReadBytes('\n')
+    if (err != nil && err != io.EOF) || len(f.next[2]) == 0 {
+        return false
+    }
+
+    // Sanitize newlines
+    f.next[0] = bytes.Trim(f.next[0], "\n")
+    f.next[1] = bytes.Trim(f.next[1], "\n")
+    f.next[2] = bytes.Trim(f.next[2], "\n")
+
+    return true
+}
+
+// Gets the next index and sequence when iterating
+func (f *FastqReader) Next() Seq {
+    return Seq{string(f.next[0][1:]), string(f.next[1]), string(f.next[2]), "", "", false}
+}
---
+++ iomama/fastq/fastq_reader_test.go
@@ -0,0 +1,39 @@
+package fasta
+
+import (
+    "bufio"
+    . "github.com/BrianReallyMany/yomama/seq"
+    "strings"
+    "testing"
+)
+
+func TestFastqReaderIteration(t *testing.T) {
+    reader := bufio.NewReader(strings.NewReader("@seq1\nATGCT\n+ blah blah  \n#$%^&\n@seq2\nATGCG\n+\n&^%$#\n@seq3\nATGCA\n+  \nABCDE"))
+
+    fastqReader := NewFastqReader(reader)
+
+    testSeqs := [...] struct {
+                          seq Seq
+                          pass bool
+                      } {
+        {Seq{"seq1", "ATGCT", "#$%^&", "", "", false}, false},
+        {Seq{"seq2", "ATGCG", "&^%$#", "", "", false}, false},
+        {Seq{"seq3", "ATGCA", "ABCDE", "", "", false}, false},
+    }
+
+    i := 0
+    for fastqReader.HasNext() {
+        seq := fastqReader.Next()
+
+        if seq == testSeqs[i].seq {
+            testSeqs[i].pass = true
+        }
+        i++
+    }
+
+    for _, seq := range testSeqs {
+        if !seq.pass {
+            t.FailNow()
+        }
+    }
+}
